ed189d97ab0c20aac8034fdd74c7961aa60f4e31
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2020-12-23 19:39:13 +0100

    Day of the Programmer

---
+++ other/day-of-the-programmer.py
@@ -0,0 +1,17 @@
+# Day of the Programmer https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+
+# Complete the dayOfProgrammer function below.
+def dayOfProgrammer(year):
+    if (year == 1918):
+        return '26.09.1918'
+    elif ((year <= 1917) & (year%4 == 0)) or ((year > 1918) & (year%400 == 0 or ((year%4 == 0) & (year%100 != 0)))):
+        return '12.09.%s' %year
+    else:
+        return '13.09.%s' %year
+
+print dayOfProgrammer(2017)
+
+print dayOfProgrammer(2016)
+
+print dayOfProgrammer(1800)
+
