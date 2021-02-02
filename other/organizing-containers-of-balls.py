b5c0b5c16294bd5afaa48c257abb9313fa042eca
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-02-02 23:09:04 +0100

    Organizing Containers of Balls

---
+++ other/organizing-containers-of-balls.py
@@ -0,0 +1,16 @@
+# Organizing Containers of Balls https://www.hackerrank.com/challenges/organizing-containers-of-balls/problem
+
+def organizingContainers(container):
+    row = [0] * n
+    col = [0] * n 
+
+    for i in range(n):
+        for j in range(n):
+            row[i] += container[i][j]
+            col[i] += container[j][i]
+
+
+    if sorted(row) == sorted(col):
+        return "Possible"
+    else:
+        return "Impossible"            
