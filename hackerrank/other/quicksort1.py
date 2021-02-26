a7c2046470f9f4e0e0d849a32c02c145b42212d4
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-02-26 22:51:06 +0100

    Quicksort 1 - Partition

---
+++ hackerrank/other/quicksort1.py
@@ -0,0 +1,25 @@
+# Quicksort 1 - Partition https://www.hackerrank.com/challenges/quicksort1/problem
+
+def quickSort(arr):
+    pivot = arr[0]
+
+    i = 0
+    j = len(arr) - 1
+
+    while i < j:
+        while i < j and arr[i] <= pivot:
+            i += 1
+
+        while arr[j] > pivot:
+            j-= 1   
+
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i] 
+            i += 1
+            j -= 1
+    arr[0] , arr[j] = arr[j], arr[0]    
+
+    return arr
+
+
+print quickSort([4, 5, 3, 7, 2])
