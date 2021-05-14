b815d96b2a2a8a14f671e9e0629a0e593f460014
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-05-14 22:34:16 +0200

    Print a given matrix in spiral form

---
+++ matrices/print-a-given-matrix-in-spiral-form.py
@@ -0,0 +1,46 @@
+# Print a given matrix in spiral form  https://www.geeksforgeeks.org/print-a-given-matrix-in-spiral-form/
+
+def spiralOrder(matrix):
+    ans = []
+
+    if not len(matrix):
+        return ans
+
+
+    R = len(matrix)
+    C = len(matrix[0])
+
+    seen = [[0 for i in range(C)] for j in range(R)]
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0] 
+
+    r = 0
+    c = 0
+    di = 0
+
+    for i in range(R * C):
+        ans.append(matrix[r])
+
+        seen[r] = True
+        cr = r + dr[di]
+        cc = c + dc[di]
+
+        if (0 <= cr and cr < R and 0 <= cc and cc < C and (not cr in seen)):
+            r = cr
+            c = cc
+        else:
+            di = (di + 1) % 4
+            r += dr[di]
+            c += dc[di]
+
+    return ans
+
+if __name__ == "__main__" : 
+    a = [[ 1, 2, 3, 4 ],
+    [ 5, 6, 7, 8 ],
+    [ 9, 10, 11, 12 ],
+    [ 13, 14, 15, 16 ]]
+ 
+    for x in spiralOrder(a):
+        print(x ,end=" ")
+    print()                    
