1100b8b1d1f6b1f98f636ac0ee690d26ca974658
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-06-17 22:32:30 +0200

    Maximum path sum in a triangle.

---
+++ dynamic-programming/exercises/maximum-path-sum-triangle.py
@@ -0,0 +1,20 @@
+# Maximum path sum in a triangle. https://www.geeksforgeeks.org/maximum-path-sum-triangle/
+
+N = 3
+
+def maxPathSum(tri, m, n):
+    for i in range(m - 1, -1, -1):
+        for j in range(i+1):
+            if tri[i +1] [j] > tri[i+1][j +1]:
+                tri[i][j] += tri[i+1][j]
+            else:
+                tri[i][j] += tri[i+1][j+1]
+
+    return tri[0][0]
+
+
+
+tri = [[1, 0, 0],
+       [4, 8, 0],
+       [1, 5, 3]]
+print(maxPathSum(tri, 2, 2))
