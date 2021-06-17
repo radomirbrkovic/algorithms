811566952f2652695ab6818963ddd4c8deb520cb
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-06-17 22:26:46 +0200

    Minimum Sum Path in a Triangle

---
+++ dynamic-programming/exercises/minimum-sum-path-triangle.py
@@ -0,0 +1,19 @@
+# Minimum Sum Path in a Triangle https://www.geeksforgeeks.org/minimum-sum-path-triangle/
+
+def minSumPath(A):
+    memo = [None] * len(A)
+    n = len(A) - 1
+
+    for i in range(len(A[n])):
+        memo[i] = A[n][i]
+
+    for i in range(len(A) - 2, -1, -1):
+        for j in range(len(A[i])):
+            memo[j] = A[i][j] + min(memo[j], memo[j + 1])
+
+    return memo[0]
+
+A = [[ 2 ],
+    [3, 9 ],
+    [1, 6, 7 ]]
+print(minSumPath(A))            
