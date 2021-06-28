d4d55b6426ccda31309002dbcbe2e88229ad3e67
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-06-28 22:40:13 +0200

    Lobb Number

---
+++ dynamic-programming/exercises/lobb-number.py
@@ -0,0 +1,27 @@
+# Lobb Number https://www.geeksforgeeks.org/lobb-number/
+
+def binomialCoeff(n, k):
+ 
+    C = [[0 for j in range(k + 1)]
+             for i in range(n + 1)]
+
+    for i in range(0, n + 1):
+        for j in range(0, min(i, k) + 1):
+            if (j == 0 or j == i):
+                C[i][j] = 1
+ 
+            else:
+                C[i][j] = (C[i - 1][j - 1]
+                            + C[i - 1][j])
+         
+    return C[n][k]
+ 
+def lobb(n, m):
+ 
+    return (((2 * m + 1) *
+        binomialCoeff(2 * n, m + n))
+                      / (m + n + 1))
+
+
+print(int(lobb(3, 2)))
+print(int(lobb(5, 3)))
